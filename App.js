b3940335014a87f245b7df356e74643ac9a75b52
smooth97 <dbtmdwls0506@naver.com>
2021-07-10 23:45:38 +0900

    #8

---
+++ App.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.body}>
+      <View style={styles.view1}>
+        <Text style={styles.text}>1</Text>
+      </View>
+      <View style={styles.view2}>
+        <Text style={styles.text}>2</Text>
+      </View>
+      <View style={styles.view3}>
+        <Text style={styles.text}>3</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#0000ff',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  view1: {
+    flex: 1,
+    backgroundColor: '#00ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view2: {
+    flex: 1,
+    backgroundColor: '#ff00ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view3: {
+    flex: 1,
+    backgroundColor: '#ffff00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#000000',
+    fontSize: 35,
+    fontStyle: 'italic',
+    margin: 10,
+  },
+});
+
+export default App;
