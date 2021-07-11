57bb2a1c0af346de19d2cda60532d958bfbe26ec
smooth97 <dbtmdwls0506@naver.com>
2021-07-11 10:57:01 +0900

    flatList & sectionList

--- App.js
+++ App.js
@@ -1,19 +1,83 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  RefreshControl,
+  FlatList,
+} from 'react-native';
 
 const App = () => {
+  const [list, setList] = useState([
+    {key: '1', name: 'item 01'},
+    {key: '2', name: 'item 02'},
+    {key: '3', name: 'item 03'},
+    {key: '4', name: 'item 04'},
+    {key: '5', name: 'item 05'},
+    {key: '6', name: 'item 06'},
+    {key: '7', name: 'item 07'},
+    {key: '8', name: 'item 08'},
+    {key: '9', name: 'item 09'},
+    {key: '10', name: 'item 10'},
+  ]);
+
+  const DATA = [
+    {
+      title: 'Title 1',
+      data: ['Item 1-1', 'Item 1-2', 'Item 1-3'],
+    },
+    {
+      title: 'Title 2',
+      data: ['Item 2-1', 'Item 2-2', 'Item 2-3'],
+    },
+    {
+      title: 'Title 3',
+      data: ['Item 3-1'],
+    },
+    {
+      title: 'Title 4',
+      data: ['Item 4-1', 'Item 4-2'],
+    },
+  ];
+
+  const [Refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setList([...list, {name: 'Item 69'}]);
+    setRefreshing(false);
+  };
+
   return (
-    <View style={styles.body}>
-      <View style={styles.view1}>
-        <Text style={styles.text}>1</Text>
-      </View>
-      <View style={styles.view2}>
-        <Text style={styles.text}>2</Text>
-      </View>
-      <View style={styles.view3}>
-        <Text style={styles.text}>3</Text>
-      </View>
-    </View>
+    <SafeAreaView style={styles.body}>
+      {/* <FlatList
+        data={list}
+        renderItem={({item}) => (
+          <View style={styles.view}>
+            <Text style={styles.text}>{item.name}</Text>
+          </View>
+        )}
+      /> */}
+      <ScrollView
+        style={styles.body}
+        refreshControl={
+          <RefreshControl
+            refreshing={Refreshing}
+            onRefresh={onRefresh}
+            colors={['#ff00ff']}
+          />
+        }>
+        {list.map(object => {
+          return (
+            <View style={styles.item} key={object.key}>
+              <Text style={styles.text}>{object.item}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -21,31 +85,17 @@ const styles = StyleSheet.create({
   body: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#0000ff',
-    alignItems: 'stretch',
-    justifyContent: 'center',
+    backgroundColor: '#ffffff',
   },
-  view1: {
-    flex: 1,
-    backgroundColor: '#00ffff',
-    alignItems: 'center',
+  item: {
+    margin: 10,
+    backgroundColor: '#4ae1fa',
     justifyContent: 'center',
-  },
-  view2: {
-    flex: 1,
-    backgroundColor: '#ff00ff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view3: {
-    flex: 1,
-    backgroundColor: '#ffff00',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   text: {
     color: '#000000',
-    fontSize: 35,
+    fontSize: 45,
     fontStyle: 'italic',
     margin: 10,
   },
