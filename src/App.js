21529f7236e311a63ba77436346ae46090abf747
ddive-sjin <bak0506@ddive.co>
2021-07-25 14:54:23 +0900

    feat: ios push noti

--- src/App.js
+++ src/App.js
@@ -1,42 +1,97 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import Home from './screens/Home';
-import Login from './screens/Login';
-import {Provider} from 'react-redux';
-import {Store} from './redux/store';
+import React, {useEffect} from 'react';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createStackNavigator} from '@react-navigation/stack';
+// import Home from './screens/Home';
+// import Login from './screens/Login';
+// import {Provider} from 'react-redux';
+// import {Store} from './redux/store';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+import {TouchableOpacity, Text} from 'react-native';
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
 
-function App() {
+// function App() {
+//   return (
+//     <Provider store={Store}>
+//       <NavigationContainer>
+//         <Stack.Navigator
+//           initialRouteName="Login"
+//           screenOptions={{
+//             headerTitleAlign: 'center',
+//             headerStyle: {
+//               backgroundColor: '#0080ff',
+//             },
+//             headerTintColor: '#ffffff',
+//             headerTitleStyle: {
+//               fontSize: 25,
+//               fontWeight: 'bold',
+//             },
+//           }}>
+//           <Stack.Screen
+//             name="Login"
+//             component={Login}
+//             options={{
+//               headerShown: false,
+//             }}
+//           />
+//           <Stack.Screen name="Home" component={Home} />
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     </Provider>
+//   );
+// }
+
+// export default App;
+
+export const App = () => {
+  // const [permissions, setPermissions] = useState({});
+
+  /**
+   * By calling this function, notification with category `userAction` will have action buttons
+   */
+  const setNotificationCategories = () => {
+    PushNotificationIOS.setNotificationCategories([
+      {
+        id: 'userAction',
+        actions: [
+          {id: 'open', title: 'Open', options: {foreground: true}},
+          {
+            id: 'ignore',
+            title: 'Desruptive',
+            options: {foreground: true, destructive: true},
+          },
+          {
+            id: 'text',
+            title: 'Text Input',
+            options: {foreground: true},
+            textInput: {buttonTitle: 'Send'},
+          },
+        ],
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    PushNotificationIOS.addEventListener('notification', onRemoteNotification);
+  });
+
+  const onRemoteNotification = notification => {
+    const actionIdentifier = notification.getActionIdentifier();
+
+    if (actionIdentifier === 'open') {
+      // Perform action based on open action
+    }
+
+    if (actionIdentifier === 'text') {
+      // Text that of user input.
+      const userText = notification.getUserText();
+      // Perform action based on textinput action
+    }
+  };
+
   return (
-    <Provider store={Store}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Login"
-          screenOptions={{
-            headerTitleAlign: 'center',
-            headerStyle: {
-              backgroundColor: '#0080ff',
-            },
-            headerTintColor: '#ffffff',
-            headerTitleStyle: {
-              fontSize: 25,
-              fontWeight: 'bold',
-            },
-          }}>
-          <Stack.Screen
-            name="Login"
-            component={Login}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
+    <TouchableOpacity onPress={setNotificationCategories}>
+      <Text>hihi</Text>
+    </TouchableOpacity>
   );
-}
-
-export default App;
+};
